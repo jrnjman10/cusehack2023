56ca5bc72b80fe40cd27a784eec04aa4cc36b2eb
jrnjman10 <126359600+jrnjman10@users.noreply.github.com>
2023-04-16 12:15:31 -0400

    Update main.py

--- SomeGame/main.py
+++ SomeGame/main.py
@@ -1,8 +1,9 @@
 import pygame
 
 
-while
-    
+while True:
+    undefined
+
 '''
 Player controls
 Images
@@ -39,4 +40,14 @@ class Player ():
        # Update the position of this object by setting the values of rect.x and rect.y
        self.rect = self.image.get_rect()
 
-class Platform ():
+# x location, y location, img width, img height, img file
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, xloc, yloc, imgw, imgh, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join('images', img)).convert()
+        self.image.convert_alpha()
+        self.image.set_colorkey(ALPHA)
+        self.rect = self.image.get_rect()
+        self.rect.y = yloc
+        self.rect.x = xloc
+
