1c020c357a4c948ae6e4b71878ca0d4add59c3a3
jrnjman10 <126359600+jrnjman10@users.noreply.github.com>
2023-04-16 12:04:56 -0400

    Summary
    This Project intends to create a service in which the user can see how man people are using certain university facilities

---
+++ SomeGame/main.py
@@ -0,0 +1,42 @@
+import pygame
+
+
+while
+    
+'''
+Player controls
+Images
+'''
+
+'''
+Player class
+
+Enemy Class
+
+Platform class
+    Moving Platforms
+    Platforms
+
+
+Someone stole otto's scooter
+he has to get his scooter
+'''
+
+
+class Player ():
+    # Constructor. Pass in the color of the block,
+    # and its x and y position
+    def __init__(self, color, width, height):
+       # Call the parent class (Sprite) constructor
+       pygame.sprite.Sprite.__init__(self)
+
+       # Create an image of the block, and fill it with a color.
+       # This could also be an image loaded from the disk.
+       self.image = pygame.Surface([width, height])
+       self.image.fill(color)
+
+       # Fetch the rectangle object that has the dimensions of the image
+       # Update the position of this object by setting the values of rect.x and rect.y
+       self.rect = self.image.get_rect()
+
+class Platform ():
